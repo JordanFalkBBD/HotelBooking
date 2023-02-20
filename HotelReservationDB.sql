a8350a9ce226d6138041b95f8b8343649f06b1b0
Jordan Falk <jordan.falk@vcontractor.co.za>
2023-02-20 11:06:30 +0200

    Added view

--- HotelReservationDB.sql
+++ HotelReservationDB.sql
@@ -108,4 +108,32 @@ GO
 --);
 --GO
 
+CREATE VIEW [dbo].[ReservationsReport]
+AS
+SELECT 	r.[ReservationID],
+		r.[ReservationDate],
+		r.[TotalPrice],
+		COUNT([Rooms].[RoomID])						AS NumberOfRooms,
+		STRING_AGG([RoomTypes].[RoomType], ', ') 	AS RoomTypes,
+		c.[LastName],
+		c.[FirstName]
+FROM [Reservations] r
+LEFT JOIN [CustomerReservations] cr
+ON r.[ReservationID] = cr.[ReservationID]
+LEFT JOIN [Customers] c
+ON cr.[CustomerID] = c.[CustomerID]
+LEFT JOIN [ReservationRooms] rr
+ON r.[ReservationID] = rr.[ReservationID]
+LEFT JOIN [Rooms]
+ON [Rooms].[RoomID] = rr.[RoomID]
+LEFT JOIN [RoomTypes]
+ON [Rooms].[RoomTypeID] = [RoomTypes].[RoomTypeID]
+WHERE 	r.[ReservationDate] <= '2022-12-31'
+AND 	r.[ReservationDate] >= '2022-01-01'
+GROUP BY 	r.[ReservationID],
+			r.[ReservationDate],
+			r.[TotalPrice],
+			c.[LastName],
+			c.[FirstName]
 
+GO
